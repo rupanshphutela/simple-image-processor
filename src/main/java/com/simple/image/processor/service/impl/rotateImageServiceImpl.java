67d24d036f4bbc2e9ec30c8bd45de16142fd4e5b
Rupansh Phutela <rupanshphutela@gmail.com>
2022-02-21 00:04:02 -0800

    added all angles code

--- src/main/java/com/simple/image/processor/service/impl/rotateImageServiceImpl.java
+++ src/main/java/com/simple/image/processor/service/impl/rotateImageServiceImpl.java
@@ -1,5 +1,7 @@
 package com.simple.image.processor.service.impl;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import org.imgscalr.Scalr;
 import org.slf4j.Logger;
@@ -34,7 +36,8 @@ public class rotateImageServiceImpl implements rotateImageService {
 	}
 }
 */   		
-    public BufferedImage ndegrees(BufferedImage imageFile, String rotateDegrees) {
+    @SuppressWarnings("removal")
+	public BufferedImage ndegrees(BufferedImage imageFile, String rotateDegrees) {
     	try {
     	Scalr.Rotation rotation = null;
     	if(rotateDegrees == null || rotateDegrees.isEmpty()) {
@@ -43,22 +46,38 @@ public class rotateImageServiceImpl implements rotateImageService {
     	else {
     		int rotateDegrees_num = Integer.parseInt(rotateDegrees);
     		rotateDegrees_num = rotateDegrees_num%360;
+    		BufferedImage image = null;
+    		
     		switch(rotateDegrees_num) {
     			case 90:
             		rotation = Scalr.Rotation.CW_90;
-            		break;
+            		image = Scalr.rotate(imageFile, rotation);
+                    imageFile.flush();
+                    return image;
     			case 180:
             		rotation = Scalr.Rotation.CW_180;
-            		break;
+            		image = Scalr.rotate(imageFile, rotation);
+                    imageFile.flush();
+                    return image;
     			case 270:
             		rotation = Scalr.Rotation.CW_270;
-            		break;
+            		image = Scalr.rotate(imageFile, rotation);
+                    imageFile.flush();
+                    return image;
     			default:
-    				return imageFile;
+    				double radians = Math.toRadians(rotateDegrees_num);
+    			    int w = (new Double(Math.abs(imageFile.getWidth() * Math.cos(radians)) + Math.abs(imageFile.getHeight() * Math.sin(radians)))).intValue();
+    			    int h = (new Double(Math.abs(imageFile.getWidth() * Math.sin(radians)) + Math.abs(imageFile.getHeight() * Math.cos(radians)))).intValue();
+    			    image = new BufferedImage(w, h, imageFile.getType());
+    			    Graphics2D graphics = image.createGraphics();
+    			    graphics.setColor(Color.WHITE);
+    			    graphics.fillRect(0, 0, w, h);
+    			    int x = -1 * (imageFile.getWidth() - w) / 2;
+    			    int y = -1 * (imageFile.getHeight() - h) / 2;
+    			    graphics.rotate(Math.toRadians(rotateDegrees_num), (w / 2), (h / 2));
+    			    graphics.drawImage(imageFile, null, x, y);
+    			    return image;
     		}
-    		BufferedImage image = Scalr.rotate(imageFile, rotation);
-            imageFile.flush();
-            return image;
     	}
     }
     	catch (Exception e) {
