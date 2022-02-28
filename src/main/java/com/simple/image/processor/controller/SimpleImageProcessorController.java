0dad94b7e292ce9ff2b6462139472a8781266941
Rupansh Phutela <rupanshphutela@gmail.com>
2022-02-28 04:40:25 -0800

    import statements cleanup

--- src/main/java/com/simple/image/processor/controller/SimpleImageProcessorController.java
+++ src/main/java/com/simple/image/processor/controller/SimpleImageProcessorController.java
@@ -19,11 +19,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartResolver;
 
